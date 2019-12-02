dc2c1d2bd443f25bfdec72acc36514ea0a0872b9
lucapu88 <caputoluca88@gmail.com>
2019-12-02 13:26:41 +0100

    inizio

---
+++ main.js
@@ -0,0 +1,1 @@
+// dare la possibilità all'utente di inviare un messaggio: scrivendo nel campo di testo in basso e premendo un pulsante, il messaggio comparirà nel contenitore della conversazione. Per fare questo punto, vi consiglio di usare la funzione clone() che abbiamo visto insieme per utilizzare un div template a mo' di "stampo".
