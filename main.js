005a5813167b957f347fb02b413c879918c5a130
lucapu88 <caputoluca88@gmail.com>
2019-12-02 20:39:15 +0100

    sistemato un pochetto il javascript e css

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 // dare la possibilità all'utente di inviare un messaggio: scrivendo nel campo di testo in basso e premendo un pulsante, il messaggio comparirà nel contenitore della conversazione. Per fare questo punto, vi consiglio di usare la funzione clone() che abbiamo visto insieme per utilizzare un div template a mo' di "stampo".
 
 $(document).ready(function() {
-//al click su chat
-$('.chat').click(function(){
+//al click su chat-send
+$('.chat-send').click(function(){
   //quando scrivo nel placeholder
   var messaggio = document.getElementById("myText").value;
   //trasferisci quello che è stato scritto in placeholder in un div con classe message template
-  $('.container-right').append('<div class="message template"></div>');
+  $('.center-right').append('<div class="message template"></div>');
+  $('.message.template').append(messaggio);
 })
-
 });
-
-// function myFunction() {
-//   var x = document.getElementById("myText").value;
-//   document.getElementById("demo").innerHTML = x;
-// }
