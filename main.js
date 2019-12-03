c0df4e2663e3c0aad6e4bab143fbd4e6e1ffcdaf
lucapu88 <caputoluca88@gmail.com>
2019-12-03 17:37:32 +0100

    primo punto del milestone2 terminato

--- main.js
+++ main.js
@@ -1,25 +1,37 @@
-// dare la possibilità all'utente di inviare un messaggio: scrivendo nel campo di testo in basso e premendo un pulsante, il messaggio comparirà nel contenitore della conversazione. Per fare questo punto, vi consiglio di usare la funzione clone() che abbiamo visto insieme per utilizzare un div template a mo' di "stampo".
+// Milestone 2
+// ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+// un “ok” come risposta, che apparirà dopo 1 secondo.
+// ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+// contatti il cui nome contiene le lettere inserite
 
 $(document).ready(function() {
   $('.chat-send').click(function(){//al click su chat-send
     inviaMessaggio(); //applico la mia funzione creata
+    $('.chat-send i').addClass('fa-microphone').removeClass('fa-paper-plane');//torna l'icona del microfono e scompare quella della freccia
+    setTimeout(risposta,1000); //richiamo la mia funzione che invierà una risposta dopo 1 secondo
   });
   $('.myText').keypress(function(event) { //quando si è in posizione dell'input e viene premuto INVIO
     if (event.which == 13) { // se viene premuto INVIO (che corrisponde al numero 13 della mappatura dei tasti)
       inviaMessaggio(); //applico la mia funzione creata
+      setTimeout(risposta,1000); //richiamo la mia funzione che invierà una risposta dopo 1 secondo
     }
   });
   $('.myText').keyup(function(event) { //quando si è in posizione dell'input e si inizia a scrivere
     var messaggio = $('.myText').val(); //recupero il contenuto dell'input
     if (messaggio.length != 0) { //se si inizia a scrivere (viene premuto un qualsiasi pulsante)
-      $('.chat-send i').removeClass('fas fa-microphone').addClass('active'); //appare l'icona della freccia e scompare quella del microfono
+      $('.chat-send i').removeClass('fa-microphone').addClass('fa-paper-plane'); //appare l'icona della freccia e scompare quella del microfono
     } else { //altrimenti se non si scrive nulla
-      $('.chat-send i').addClass('fas fa-microphone').removeClass('active'); //resta l'icona del microfono e scompare quella della freccia
+      $('.chat-send i').addClass('fa-microphone').removeClass('fa-paper-plane'); //resta l'icona del microfono e scompare quella della freccia
     }
   });
 });
 
 
+
+function risposta() {
+  var messaggioRicevuto = $('.template2 .message-recevied').clone();//clono il template del messaggio
+  $('.center-right').append(messaggioRicevuto);//inserisco il messaggio all'interno del container
+  }
 function inviaMessaggio() {
   var messaggio = $('.myText').val();  //recupero il contenuto dell'input
   if (messaggio.length != 0) {
